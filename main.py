642ea14a448dc9ce43d3fc5e3b92638e0c987762
Ismaili Lichinga <ismaili.lichinga@tra.go.tz>
2023-04-21 17:15:56 +0300

    add audio to text

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from tasks.tasks import convert_text_to_audio
+from tasks.tasks import convert_audio_to_text, convert_text_to_audio
 
 app = FastAPI(title="Text to Audio App", docs_url="/docs", redoc_url=None)
 
 @app.post('/convert/{text}/{audio_name}',tags=["Convertion"])
 def convert(text:str,audio_name:str):
     return convert_text_to_audio(text,audio_name)
+
+@app.post('/convert-audio-to-text',tags=["Convertion"])
+def convert_audio_to_text_():
+    return convert_audio_to_text()
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,13 +1,29 @@
 from gtts import gTTS
 from playsound import playsound
+import speech_recognition as sr
 
 def convert_text_to_audio(text: str, name:str):
-    audio =  name + ".mp3"
+    audio =  name + ".wav"
 
     # by default we use english
-    language = "en"
+    language = "sw"
 
     convert = gTTS(text=text,lang=language,slow=False)
     convert.save(audio)
     playsound(audio)
+    
+    
+def convert_audio_to_text():
+    recognizer = sr.Recognizer()
+    language = "sw"
+    with sr.AudioFile('ismailiSw.wav') as source:
+    
+       audio_text = recognizer.listen(source)
+       try:
+            # using google speech recognition
+            text = recognizer.recognize_google(audio_text)
+            print('Converting audio transcripts into text ...')
+            print(text)
+       except:
+            print('Sorry.. run again...')
     
